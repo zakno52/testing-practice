c664ce27c7d7b64e2f604d017af98e382604a705
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-07-30 00:17:55 +0100

    implement calculator and 1st stage of caesarCipher

--- code.js
+++ code.js
@@ -1,7 +1,44 @@
 export function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1);
+  if (str) {
+    return str[0].toUpperCase() + str.slice(1);
+  } else {
+    return '';
+  }
 }
 
 export function reverseString(str) {
   return str.split('').reverse().join('');
+}
+
+export const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+  divide(num1, num2) {
+    return num1 / num2;
+  },
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
+};
+
+export function caesarCipher(str, num) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let letters = [];
+  let results = [];
+
+  for (let i = 0; i < 26 + num; i++) {
+    letters.push(alphabet[i % 26]);
+  }
+
+  for (let index = 0; index < str.length; index++) {
+    let indexOfTarget = letters.indexOf(str[index]);
+    let newLetter = letters[parseInt(indexOfTarget) + num];
+    results.push(newLetter);
+  }
+
+  return results.join('');
 }
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './code';
+import { capitalize, reverseString, calculator, caesarCipher } from './code';
 
 describe('capitalize function', () => {
   test('capitalizes the first letter of a lowercase string', () => {
@@ -15,5 +15,26 @@ describe('reverseString function', () => {
   });
   test('handles empty string', () => {
     expect(reverseString('')).toBe('');
+  });
+});
+
+describe('calculator function', () => {
+  test('sum function - add two numbers', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+  test('subtract function - sub two numbers', () => {
+    expect(calculator.subtract(1, 2)).toBe(-1);
+  });
+  test('divide function - sub two numbers', () => {
+    expect(calculator.divide(1, 2)).toBe(0.5);
+  });
+  test('multiply function - sub two numbers', () => {
+    expect(calculator.multiply(1, 2)).toBe(2);
+  });
+});
+
+describe('caesarCipher function', () => {
+  test('simple encryption lowercase', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
   });
 });
